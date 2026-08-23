d45eedd79106bd6d7a636706548cf3358b583973
Vastosine <2261819266@qq.com>
2026-08-23 15:16:51 +0800

    BigraphMatch RecipeMatches

---
+++ src/main/java/com/vastosine/obsidian/utils/BigraphMatch.java
@@ -0,0 +1,70 @@
+package com.vastosine.obsidian.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BigraphMatch {
+    private final List<List<Integer>> l, r;
+    private int[][] w;
+    private final int[] lCount, rCount;
+    private int[] matches;
+    private final int ln, rn;
+
+    public BigraphMatch(int ln, int rn) {
+        this.ln = ln;
+        this.rn = rn;
+        l = new ArrayList<>();
+        r = new ArrayList<>();
+        for (int i = 0; i < ln; i++) {
+            l.add(new ArrayList<>());
+        }
+        for (int i = 0; i < rn; i++) {
+            r.add(new ArrayList<>());
+        }
+        lCount = new int[ln];
+        rCount = new int[rn];
+    }
+
+    public void addEdge(int u, int v) {
+        l.get(u).add(v);
+        r.get(v).add(u);
+    }
+
+    public void setCount(boolean isRight, int index, int count) {
+        (isRight ? rCount : lCount)[index] = count;
+    }
+
+    private int dfs(boolean side, int u, int count, boolean[] lv, boolean[] rv) {
+        boolean[] vis = side ? rv : lv;
+        if (count <= 0 || vis[u]) return 0;
+        vis[u] = true;
+        int ret = 0;
+        if (side) {
+            ret = Math.min(count, rCount[u] - matches[u]);
+            matches[u] += ret;
+            count -= ret;
+        }
+        for (int v : (side ? r : l).get(u)) {
+            if (count <= 0) break;
+            int x = dfs(!side, v, side ? Math.min(count, w[v][u]) : count, lv, rv);
+            ret += x;
+            count -= x;
+            if (side) w[v][u] -= x;
+            else w[u][v] += x;
+        }
+        return ret;
+    }
+
+    public int[] run() {
+        w = new int[ln][rn];
+        matches = new int[rn];
+        for (int i = 0; i < ln; i++) {
+            int count = lCount[i], x;
+            while ((x = dfs(false, i, count, new boolean[ln], new boolean[rn])) > 0) {
+                count -= x;
+            }
+            if (count > 0) return null;
+        }
+        return matches;
+    }
+}
---
+++ src/main/java/com/vastosine/obsidian/utils/RecipeMatches.java
@@ -0,0 +1,31 @@
+package com.vastosine.obsidian.utils;
+
+import com.vastosine.obsidian.item.crafting.IngredientWithCount;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeInput;
+
+import java.util.List;
+
+public class RecipeMatches {
+
+    public static <I extends RecipeInput> int[] getMatches(final List<IngredientWithCount> ingredients, final I inputs) {
+        int n = ingredients.size();
+        int m = inputs.size();
+        BigraphMatch graph = new BigraphMatch(n, m);
+        for (int i = 0; i < n; i++) {
+            Ingredient ingredient = ingredients.get(i).ingredient();
+            graph.setCount(false, i, ingredients.get(i).count());
+            for (int j = 0; j < m; j++) {
+                ItemStack input = inputs.getItem(j);
+                if (ingredient.test(input)) {
+                    graph.addEdge(i, j);
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            graph.setCount(true, i, inputs.getItem(i).count());
+        }
+        return graph.run();
+    }
+}
