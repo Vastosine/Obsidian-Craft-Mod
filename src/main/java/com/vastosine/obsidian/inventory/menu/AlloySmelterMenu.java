620fb3f46026388a2736b42b3a43d5a4e1c3a67f
Vastosine <2261819266@qq.com>
2026-08-26 09:51:34 +0800

    Release version 0.2-26.3-fabric

--- src/main/java/com/vastosine/obsidian/inventory/menu/AlloySmelterMenu.java
+++ src/main/java/com/vastosine/obsidian/inventory/menu/AlloySmelterMenu.java
@@ -12,10 +12,10 @@ public class AlloySmelterMenu extends AbstractAlloySmelterMenu{
     public static final int SLOT_RESULT_COUNT = 1;
 
     public AlloySmelterMenu(final int containerId, final Inventory inventory) {
-        super(OCMenuTypes.ALLOY_SMELTER, RecipePropertySet.FURNACE_INPUT, RecipeBookType.FURNACE, containerId, inventory, SLOT_INPUT_COUNT, SLOT_FUEL_COUNT, SLOT_RESULT_COUNT);
+        super(OCMenuTypes.ALLOY_SMELTER, RecipeBookType.FURNACE, containerId, inventory, SLOT_INPUT_COUNT, SLOT_FUEL_COUNT, SLOT_RESULT_COUNT);
     }
 
     public AlloySmelterMenu(final int containerId, final Inventory inventory, final Container container, final ContainerData data) {
-        super(OCMenuTypes.ALLOY_SMELTER, RecipePropertySet.FURNACE_INPUT, RecipeBookType.FURNACE, containerId, inventory, container, data, SLOT_INPUT_COUNT, SLOT_FUEL_COUNT, SLOT_RESULT_COUNT);
+        super(OCMenuTypes.ALLOY_SMELTER, RecipeBookType.FURNACE, containerId, inventory, container, data, SLOT_INPUT_COUNT, SLOT_FUEL_COUNT, SLOT_RESULT_COUNT);
     }
 }
